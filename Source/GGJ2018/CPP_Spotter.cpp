0d7f266c0861eb3773df38da524909739851d31a
Tim Patullock <oddalicious@gmail.com>
2018-04-21 01:35:44 +0800

    Moving code from Blueprint to C++ where applicable. Spotter and CupCharacter are my primary changes so far, I've also fiddled with the CupCharacter's Anim Blueprint (CupAnimInstance) in case I decide to move that down.
    Currently Circular Dependencies are a worry so I might put the anim till the end and decide once I know where the majority of my code-base is.

--- Source/GGJ2018/CPP_Spotter.cpp
+++ Source/GGJ2018/CPP_Spotter.cpp
@@ -1,6 +1,9 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "CPP_Spotter.h"
+#include "Engine.h"
+#include "Runtime/Engine/Public/DrawDebugHelpers.h"
+#include "Runtime/Engine/Classes/Kismet/KismetMathLibrary.h"
 
 
 // Sets default values
@@ -8,14 +11,21 @@ ACPP_Spotter::ACPP_Spotter()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
+	defaultRoot = CreateDefaultSubobject<USceneComponent>(TEXT("DefaultRoot"));
+	RootComponent = defaultRoot;
+	skeletalMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("SkeletalMesh"));
+	skeletalMesh->SetupAttachment(RootComponent);
+	spotLight = CreateDefaultSubobject<USpotLightComponent>(TEXT("SpotLight"));
+	spotLight->SetupAttachment(skeletalMesh);
+	regularTarget = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("RegularTarget"));
+	regularTarget->SetupAttachment(RootComponent);
 }
 
 // Called when the game starts or when spawned
 void ACPP_Spotter::BeginPlay()
 {
 	Super::BeginPlay();
-	
+	SubscribeTriggers();
 }
 
 // Called every frame
@@ -25,3 +35,127 @@ void ACPP_Spotter::Tick(float DeltaTime)
 
 }
 
+void ACPP_Spotter::SubscribeTriggers()
+{
+	for (ATriggerBox* box : triggerZones)
+	{
+		if (box)
+		{
+			box->OnActorBeginOverlap.AddUniqueDynamic(this,&ACPP_Spotter::ActorBeginOverlap);
+			box->OnActorEndOverlap.AddUniqueDynamic(this,&ACPP_Spotter::ActorEndOverlap);
+		}
+	}
+}
+
+void ACPP_Spotter::ActorBeginOverlap(AActor* OverlappedActor, AActor* OtherActor)
+{
+	ACPP_CupCharacter* cup = dynamic_cast<ACPP_CupCharacter*>(OtherActor);
+	if (cup)
+	{
+		cupCharacter = cup;	
+		activeTriggers++;
+
+		FString triggerText = "Trigger Connected: " + activeTriggers;
+
+		if (GEngine)
+		{
+			GEngine->AddOnScreenDebugMessage(0, 1.f, FColor::Cyan, triggerText);
+		}
+	}
+}
+
+void ACPP_Spotter::ActorEndOverlap(AActor* OverlappedActor, AActor* OtherActor)
+{
+	ACPP_CupCharacter* cup = dynamic_cast<ACPP_CupCharacter*>(OtherActor);
+	if (!cup)
+	{
+		return;
+	}
+
+	cupCharacter = cup;
+	activeTriggers = FMath::Max(activeTriggers - 1, 0);
+	if (activeTriggers == 0)
+	{
+		cupCharacter = nullptr;
+	}	
+	FString triggerText = "Trigger Disconnected: " + activeTriggers;
+
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(0,1.f,FColor::Orange, triggerText);
+	}
+}
+
+void ACPP_Spotter::CheckPlayerLoss()
+{
+	if (currentSpotTime >= lossTime)
+	{
+		if (cupCharacter != nullptr)
+		{
+			cupCharacter->ShowLoseWidget();
+		}
+	}
+}
+
+void ACPP_Spotter::SpottingLogicSplit(TriggerLogicSplitEnum& Branches)
+{
+	if (AnyTriggersActive())
+	{
+		if (IsPlayerVisible() && IsPlayerNoticeable())
+		{
+			Branches = TriggerLogicSplitEnum::TriggersActiveAndNoticeable;
+		}
+		else
+		{
+			Branches = TriggerLogicSplitEnum::TriggersActiveAndHidden;
+		}
+	}
+	else
+	{
+		Branches = TriggerLogicSplitEnum::TriggersInactive;
+	}
+
+
+}
+
+bool ACPP_Spotter::IsPlayerVisible()
+{
+	if (!AnyTriggersActive())
+	{
+		return false;
+	}
+
+	int hitRays = 0;
+	FVector direction;
+	FVector spotLocation = spotLight->K2_GetComponentLocation();
+	FRotator towardsCup = UKismetMathLibrary::FindLookAtRotation( spotLocation,cupCharacter->GetActorLocation());
+	float halfRadius = cupCharacter->GetCapsuleComponent()->GetUnscaledCapsuleRadius();
+	FVector rightVector = UKismetMathLibrary::GetRightVector(towardsCup) * halfRadius;
+
+	FHitResult outResult;
+
+	for (int ray : rays)
+	{
+		direction = cupCharacter->GetActorLocation() + (rightVector * ray);
+		
+		if (GetWorld()->LineTraceSingleByChannel(outResult, spotLocation, direction,ECollisionChannel::ECC_Visibility))
+		{
+			if (outResult.Actor == cupCharacter)
+			{
+				DrawDebugLine(GetWorld(), spotLocation, direction, FColor::Red);
+				hitRays++;
+			}
+			else
+			{
+				DrawDebugLine(GetWorld(), spotLocation, direction, FColor::Green);
+			}
+		}
+		else
+		{
+			DrawDebugLine(GetWorld(), spotLocation, direction, FColor::Green);
+		}
+	}
+	
+
+	return hitRays > 1;
+}
--- Source/GGJ2018/CPP_Spotter.h
+++ Source/GGJ2018/CPP_Spotter.h
@@ -3,8 +3,19 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Runtime/Engine/Classes/Engine/TriggerBox.h"
+#include "CPP_CupCharacter.h"
 #include "GameFramework/Actor.h"
+#include "Runtime/Engine/Classes/Components/SpotLightComponent.h"
 #include "CPP_Spotter.generated.h"
+
+UENUM(BlueprintType)
+enum class TriggerLogicSplitEnum : uint8
+{
+	TriggersActiveAndNoticeable,
+	TriggersActiveAndHidden,
+	TriggersInactive,
+};
 
 UCLASS()
 class GGJ2018_API ACPP_Spotter : public AActor
@@ -19,10 +30,78 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	void SubscribeTriggers();
+	void ActorBeginOverlap(AActor* OverlappedActor, AActor* OtherActor);
+	void ActorEndOverlap(AActor* OverlappedActor, AActor* OtherActor);
+
+	UFUNCTION(BlueprintCallable)
+	void CheckPlayerLoss();
+
+	UFUNCTION(BlueprintPure)
+	bool AnyTriggersActive() const
+	{
+		return activeTriggers > 0;
+	}
+
+	UFUNCTION(BlueprintPure)
+	bool IsPlayerNoticeable() const
+	{
+		if (cupCharacter)
+		{
+			return cupCharacter->isRotating || cupCharacter->isMoving || cupCharacter->IsAnimating();
+		}
+		return false;
+	}
+
+	UFUNCTION(BlueprintCallable, Category = CustomNodes,  meta = (ExpandEnumAsExecs = Branches))
+	void SpottingLogicSplit(TriggerLogicSplitEnum& Branches);
+	UFUNCTION(BlueprintCallable)
+	bool IsPlayerVisible();
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	USpotLightComponent* spotLight = nullptr;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	USkeletalMeshComponent* skeletalMesh = nullptr;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	UStaticMeshComponent* regularTarget = nullptr;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	USceneComponent* defaultRoot = nullptr;
+
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
-	
-	
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	float timeToNoticePlayer = 0.7f;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	float timeSinceSpot = 0.0f;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	float lossTime = 3.2f;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	float reduceSpeed = 2.0f;
+	UPROPERTY(BlueprintReadWrite)
+	float currentSpotTime = 0.0f;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	TArray<float> rays = {-1.f, -.5f, 0.f, .5f, 1.f};
+	UPROPERTY(BlueprintReadWrite)
+	bool canBeAnnoyed = false;
+	UPROPERTY(BlueprintReadWrite)
+	int activeTriggers = 0;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	TArray<USoundBase*> huhSounds;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	TArray<USoundBase*> giveUpSounds;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	TArray<ATriggerBox*> triggerZones;
+	UPROPERTY(BlueprintReadWrite)
+	ACPP_CupCharacter* cupCharacter = nullptr;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	FLinearColor safeColor = FLinearColor::Green;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	FLinearColor dangerColor = FLinearColor::Red;
+	UPROPERTY(BlueprintReadWrite)
+	bool spotting = false;
 };
